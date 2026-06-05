7f4f560fc65f2f06c2ef0fc464828ba98f6c7be5
prasenjitpaul-coder <prasenjitpaul.coder@gmail.com>
2026-06-05 11:37:53 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -85,7 +85,8 @@ exports.login = async (req, res, next) => {
     const cookieOptions = {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production'
+      secure: true,
+      sameSite: "none"
     };
 
     // 9. Send response with cookie and json data
