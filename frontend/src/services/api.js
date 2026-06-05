c8d5ec7977afbafd840269390e547af6eb84827d
prasenjitpaul-coder <prasenjitpaul.coder@gmail.com>
2026-06-05 10:36:44 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://vms-new-98q5.onrender.com/api',
   withCredentials: true, // For cookie-based JWT passing
 });
 
 export default api;
+
